f5eec4191c596605870fa103c7100b4191a2bf16
CSharpTeoMan911 <teodormihail07@gmail.com>
2023-03-09 23:58:46 +0000

    Add project files.

---
+++ Omega_Drive_Server/Server_Cryptographic_Functions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omega_Drive_Server
+{
+    class Server_Cryptographic_Functions
+    {
+        protected static Task<bool> Create_X509_Server_Certificate()
+        {
+            //System.Security.Cryptography.X509Certificates.X509Certificate
+
+            return Task.FromResult(true);
+        }
+    }
+}
