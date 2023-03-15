83df244c1759f9fd3f9678a54fd146ec07bf463d
CSharpTeoMan911 <teodormihail07@gmail.com>
2023-03-15 18:11:34 +0000

    * CREATED SERVER CERTIFICATE AUTHORITY IN-APP UTILITY

--- Omega_Drive_Server/Program.cs
+++ Omega_Drive_Server/Program.cs
@@ -6,7 +6,10 @@ namespace Omega_Drive_Server
     {
         static void Main(string[] args)
         {
-            
+
+
+
+            Server_Cryptographic_Functions.Create_X509_Server_Certificate("OMEGA", 365);
         }
     }
 }
--- Omega_Drive_Server/Server_Cryptographic_Functions.cs
+++ Omega_Drive_Server/Server_Cryptographic_Functions.cs
@@ -1,18 +1,143 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Omega_Drive_Server
 {
     class Server_Cryptographic_Functions
     {
-        protected static Task<bool> Create_X509_Server_Certificate()
-        {
-            //System.Security.Cryptography.X509Certificates.X509Certificate
 
-            return Task.FromResult(true);
+
+        public static async Task<bool> Create_X509_Server_Certificate(string password, int certificate_valid_time_period_in_days)
+        {
+
+            bool server_certificate_creation_successful = false;
+
+
+            try
+            {
+                Org.BouncyCastle.Crypto.Prng.CryptoApiRandomGenerator randomGenerator = new Org.BouncyCastle.Crypto.Prng.CryptoApiRandomGenerator();
+                Org.BouncyCastle.Security.SecureRandom random = new Org.BouncyCastle.Security.SecureRandom(randomGenerator);
+
+
+                Org.BouncyCastle.X509.X509V3CertificateGenerator certificateGenerator = new Org.BouncyCastle.X509.X509V3CertificateGenerator();
+
+
+                Org.BouncyCastle.Math.BigInteger serialNumber = Org.BouncyCastle.Utilities.BigIntegers.CreateRandomInRange(Org.BouncyCastle.Math.BigInteger.One, Org.BouncyCastle.Math.BigInteger.ValueOf(int.MaxValue), random);
+                certificateGenerator.SetSerialNumber(serialNumber);
+
+
+
+                Org.BouncyCastle.Asn1.X509.X509Name subjectDN = new Org.BouncyCastle.Asn1.X509.X509Name("CN=Omega_Drive_Certificate");
+                Org.BouncyCastle.Asn1.X509.X509Name issuerDN = subjectDN;
+                certificateGenerator.SetIssuerDN(issuerDN);
+                certificateGenerator.SetSubjectDN(subjectDN);
+
+
+
+                DateTime notBefore = DateTime.UtcNow.Date;
+                DateTime notAfter = notBefore.AddDays(certificate_valid_time_period_in_days);
+
+                certificateGenerator.SetNotBefore(notBefore);
+                certificateGenerator.SetNotAfter(notAfter);
+
+
+
+                const int strength = 2048;
+                Org.BouncyCastle.Crypto.KeyGenerationParameters keyGenerationParameters = new Org.BouncyCastle.Crypto.KeyGenerationParameters(random, strength);
+
+                Org.BouncyCastle.Crypto.Generators.RsaKeyPairGenerator keyPairGenerator = new Org.BouncyCastle.Crypto.Generators.RsaKeyPairGenerator();
+                keyPairGenerator.Init(keyGenerationParameters);
+                Org.BouncyCastle.Crypto.AsymmetricCipherKeyPair subjectKeyPair = keyPairGenerator.GenerateKeyPair();
+
+                certificateGenerator.SetPublicKey(subjectKeyPair.Public);
+
+
+                Org.BouncyCastle.Crypto.ISignatureFactory signatureFactory = new Org.BouncyCastle.Crypto.Operators.Asn1SignatureFactory("SHA256WithRSA", subjectKeyPair.Private, random);
+
+
+
+                Org.BouncyCastle.X509.X509Certificate certificate = certificateGenerator.Generate(signatureFactory);
+
+
+
+                Org.BouncyCastle.Pkcs.Pkcs12Store store = new Org.BouncyCastle.Pkcs.Pkcs12Store();
+
+
+                string friendlyName = certificate.SubjectDN.ToString();
+
+
+
+                Org.BouncyCastle.Pkcs.X509CertificateEntry certificateEntry = new Org.BouncyCastle.Pkcs.X509CertificateEntry(certificate);
+                store.SetCertificateEntry(friendlyName, certificateEntry);
+
+
+
+
+                store.SetKeyEntry(friendlyName, new Org.BouncyCastle.Pkcs.AsymmetricKeyEntry(subjectKeyPair.Private), new[] { certificateEntry });
+
+
+
+
+
+                System.IO.MemoryStream certificate_memory_stream = new System.IO.MemoryStream();
+
+                try
+                {
+                    store.Save(certificate_memory_stream, password.ToCharArray(), random);
+                    await certificate_memory_stream.FlushAsync();
+
+
+                    System.IO.FileStream certificate_file_stream = System.IO.File.Create("Omega_Drive.crt", (int)certificate_memory_stream.Length, System.IO.FileOptions.Asynchronous);
+                    try
+                    {
+                        await certificate_file_stream.WriteAsync(certificate_memory_stream.ToArray());
+                        await certificate_file_stream.FlushAsync();
+                    }
+                    catch
+                    {
+                        if (certificate_file_stream != null)
+                        {
+                            await certificate_file_stream.FlushAsync();
+                            certificate_file_stream.Close();
+                        }
+                    }
+                    finally
+                    {
+                        if (certificate_file_stream != null)
+                        {
+                            await certificate_file_stream.FlushAsync();
+                            certificate_file_stream.Close();
+                            await certificate_file_stream.DisposeAsync();
+                        }
+                    }
+                }
+                catch
+                {
+                    if (certificate_memory_stream != null)
+                    {
+                        await certificate_memory_stream.FlushAsync();
+                        certificate_memory_stream.Close();
+                    }
+                }
+                finally
+                {
+                    if (certificate_memory_stream != null)
+                    {
+                        await certificate_memory_stream.FlushAsync();
+                        certificate_memory_stream.Close();
+                        await certificate_memory_stream.DisposeAsync();
+                    }
+                }
+
+                server_certificate_creation_successful = true;
+            }
+            catch
+            {
+                
+            }
+
+
+            return server_certificate_creation_successful;
         }
     }
 }
